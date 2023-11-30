887311ead786606ca34a0b6a3cf41aeb3e810819
pgentil <pgentil@ucm.es>
2023-11-30 19:44:27 +0100

    pepe

---
+++ tp1/logic/gameobjects/Ship.java
@@ -0,0 +1,17 @@
+package tp1.logic.gameobjects;
+
+import tp1.logic.Game;
+import tp1.logic.Move;
+import tp1.logic.Position;
+
+public abstract class Ship extends GameObject{
+	protected Move dir;
+
+	public Ship(Game game, Position pos, int life) {
+		super(game, pos, life);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
